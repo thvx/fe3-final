f788e8fe256cedd389461629b094395d563c7fbb
Dafna Nicole <nicole26_12@hotmail.com>
2024-12-06 22:45:16 -0500

    Add: route creation

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,25 @@
-
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Contact from "./Routes/Contact";
+import Detail from "./Routes/Detail";
+import Favs from "./Routes/Favs";
 import Footer from "./Components/Footer";
+import Home from "./Routes/Home";
 import Navbar from "./Components/Navbar";
-
 
 function App() {
   return (
+    <Router>
       <div className="App">
           <Navbar/>
+          <Routes>
+            <Route path="/home" element={<Home />}/>
+            <Route path="/contacto" element={<Contact />}/>
+            <Route path="/dentist/:id" element={<Detail />}/>
+            <Route path="/favs" element={<Favs />}/>
+          </Routes>
           <Footer/>
       </div>
+    </Router>
   );
 }
 
