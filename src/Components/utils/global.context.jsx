9b5b21dbc6025e84c5a8b4d728a06cddb1649e68
Dafna Nicole <nicole26_12@hotmail.com>
2024-12-07 04:13:03 -0500

    Feat. global context implemented

--- src/Components/utils/global.context.jsx
+++ src/Components/utils/global.context.jsx
@@ -1,14 +1,48 @@
-import { createContext } from "react";
+import { createContext, useReducer, useEffect } from "react";
 
-export const initialState = {theme: "", data: []}
+export const ContextGlobal = createContext();
 
-export const ContextGlobal = createContext(undefined);
+const initialState = { 
+  theme: localStorage.getItem("theme") || "light", 
+  data: [], 
+  favorites: [] 
+};
+
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "SET_THEME":
+      localStorage.setItem("theme", action.payload);
+      return { ...state, theme: action.payload };
+    case "SET_DATA":
+      return { ...state, data: action.payload };
+    case "ADD_FAV":
+      const updatedFavs = [...state.favorites, action.payload];
+      localStorage.setItem("favorites", JSON.stringify(updatedFavs));
+      return { ...state, favorites: updatedFavs };
+    case "LOAD_FAVS":
+      return { ...state, favorites: action.payload };
+    default:
+      return state;
+  }
+};
 
 export const ContextProvider = ({ children }) => {
-  //Aqui deberan implementar la logica propia del Context, utilizando el hook useMemo
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  useEffect(() => {
+    // Fetch data from API
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => response.json())
+      .then((data) => dispatch({ type: "SET_DATA", payload: data }));
+      
+    // Load favorites from localStorage
+    const storedFavs = JSON.parse(localStorage.getItem("favorites")) || [];
+    dispatch({ type: "LOAD_FAVS", payload: storedFavs });
+  }, []);
 
   return (
-    <ContextGlobal.Provider value={{}}>
+    <ContextGlobal.Provider value={{ state, dispatch }}>
       {children}
     </ContextGlobal.Provider>
   );
